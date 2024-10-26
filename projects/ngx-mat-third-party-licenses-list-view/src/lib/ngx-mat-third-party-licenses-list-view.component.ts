0689fd0cc60ba9bad92a2261218d321eef65046d
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-10-26 12:05:14 +0900

    update: ngx-mat-third-party-licenses-list-view
    add ripple to expantion panel

--- projects/ngx-mat-third-party-licenses-list-view/src/lib/ngx-mat-third-party-licenses-list-view.component.ts
+++ projects/ngx-mat-third-party-licenses-list-view/src/lib/ngx-mat-third-party-licenses-list-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { License } from './model/license';
@@ -6,7 +7,7 @@ import { License } from './model/license';
 @Component({
   selector: 'ngx-mat-third-party-licenses-list-view',
   standalone: true,
-  imports: [MatExpansionModule, MatIconModule],
+  imports: [MatRippleModule, MatExpansionModule, MatIconModule],
   templateUrl: './ngx-mat-third-party-licenses-list-view.component.html',
   styleUrl: './ngx-mat-third-party-licenses-list-view.component.css',
 })
